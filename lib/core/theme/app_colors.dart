c57fe1592244444e0c26a0cb1df40957fab3f2cd
Guilherme Esteves <guiresteves@gmail.com>
2026-05-26 02:49:11 -0300

    feat(task): implementa mais cores do projeto

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -14,4 +14,32 @@ class AppColors {
     static const textPrimary = Colors.black;
     static const textMuted = Color(0xFF7E7777);
     static const textOnDark = Colors.white;
+
+    // Cores dos Avatares
+
+    static const avatarPink = Color(0xFFE57FAA);
+    static const avatarBlue = Color(0xFF7B9FE0);
+    static const avatarTeal = Color(0xFF6DC4A8);
+
+    // Cards e Contêineres
+
+    static const cardBackground = Colors.white;
+    static const divider = Color(0xFFF0F0F0);
+    static const checkboxBorder = Color(0xFFBBBBBB);
+
+    // Cores de Fundo
+
+    static const gradientStart = Colors.white;
+    static const gradientEnd = Color(0xFFBDEDF3);
+    
+    static const backgroundGradient = LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [gradientStart, gradientEnd],
+    );
+
+    // Erros e Alertas
+
+    static const error = Colors.redAccent;
+    static const inputFocusBorder = primaryBlue;
 }
