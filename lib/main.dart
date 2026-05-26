12abbda804783a05529f9d8e569b71cd0bbe6b5b
Gustavo Menezes Carvalho <gmcarvalho@sefaz.se.gov.br>
2026-05-26 16:40:35 -0300

    Integra telas home e grupo navegaveis

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
-import 'package:agendacare/presentation/pages/history_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
-import 'presentation/pages/welcome_page.dart';
+import 'presentation/pages/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (_) {
+    // O front-end precisa abrir no navegador mesmo antes da configuracao web do Firebase.
+  }
   runApp(const MyApp());
 }
 
@@ -18,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),//
+      home: HomePage(),
     );
   }
 }
