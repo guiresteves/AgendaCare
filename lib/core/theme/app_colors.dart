29639b42e9885b4562dd8d32764de485ef8f4694
Guilherme Esteves <guiresteves@gmail.com>
2026-05-26 02:09:09 -0300

    feat(task): adiciona classe AppColor para padronizar as cores do projeto

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+    AppColors._();
+
+    // Cores Principais
+
+    static const primaryBlue = Color(0xFF4195CC);
+    static const confirmGreen = Color(0xFF5DBB72);
+
+    // Textos
+
+    static const textPrimary = Colors.black;
+    static const textMuted = Color(0xFF7E7777);
+    static const textOnDark = Colors.white;
+}
